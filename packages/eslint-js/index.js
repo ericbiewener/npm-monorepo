32f9b5e3865eb65841a7daa574c3d6e18762d3b3
Eric Biewener <eric.biewener0@walmart.com>
2022-07-06 13:19:46 -0700

    more - 1:19 pm - Jul 6

--- packages/eslint-js/index.js
+++ packages/eslint-js/index.js
@@ -1,56 +1,58 @@
 module.exports = {
-  extends: ['eslint:recommended', 'prettier'],
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module',
-  },
-  rules: {
-    'linebreak-style': [2, 'unix'],
-    quotes: [2, 'single', { avoidEscape: true }],
-    semi: [2, 'never'],
-    'no-console': 0,
-    'prefer-const': 2,
-    'object-curly-spacing': [2, 'always'],
-    'comma-dangle': [
-      2,
-      {
-        arrays: 'always-multiline',
-        objects: 'always-multiline',
-        imports: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'never',
-      },
-    ],
-    'no-cond-assign': 0,
-    'no-throw-literal': 2,
-    'no-unused-vars': [2, { ignoreRestSiblings: true }],
-    'no-unused-expressions': 2,
-    'no-unexpected-multiline': 2,
-    'no-prototype-builtins': 0,
-    'max-len': [
-      2,
-      {
-        code: 100,
-        ignoreComments: true,
-        ignoreTrailingComments: true,
-        ignoreUrls: true,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true,
-        ignoreRegExpLiterals: true,
-      },
-    ],
+  extends: ["eslint:recommended", "prettier"],
+  overrideConfig: {
+    parser: "babel-eslint",
+    parserOptions: {
+      ecmaVersion: 2017,
+      sourceType: "module",
+    },
+    rules: {
+      "linebreak-style": [2, "unix"],
+      quotes: [2, "single", { avoidEscape: true }],
+      semi: [2, "never"],
+      "no-console": 0,
+      "prefer-const": 2,
+      "object-curly-spacing": [2, "always"],
+      "comma-dangle": [
+        2,
+        {
+          arrays: "always-multiline",
+          objects: "always-multiline",
+          imports: "always-multiline",
+          exports: "always-multiline",
+          functions: "never",
+        },
+      ],
+      "no-cond-assign": 0,
+      "no-throw-literal": 2,
+      "no-unused-vars": [2, { ignoreRestSiblings: true }],
+      "no-unused-expressions": 2,
+      "no-unexpected-multiline": 2,
+      "no-prototype-builtins": 0,
+      "max-len": [
+        2,
+        {
+          code: 100,
+          ignoreComments: true,
+          ignoreTrailingComments: true,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreRegExpLiterals: true,
+        },
+      ],
+    },
   },
   overrides: [
     {
-      files: ['**/*.test.{js,jsx}', '**/testUtils.{js,jsx}'],
+      files: ["**/*.test.{js,jsx}", "**/testUtils.{js,jsx}"],
       env: {
         jest: true,
       },
       rules: {
-        'no-empty': 0,
-        'max-nested-callbacks': 0,
+        "no-empty": 0,
+        "max-nested-callbacks": 0,
       },
     },
   ],
-}
+};
